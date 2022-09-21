0dbc3e051b08566c5faf1d25b13764ecc8768f68
George Stevenson <george.stevenson.22@gmail.com>
2022-09-20 21:34:41 -0400

    adding routing to reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -21,8 +21,43 @@ router.get('/:reviewId',
 );
 
 router.post('/:reviewId/images',
+    requireAuth,
     async (req, res) => {
+        //beginning of async
+        const userId = req.user.id
+        const reviewId = req.params.reviewId
+        const review = await Review.findByPk(reviewId)
 
+        /*Image Table
+        url
+        imageableType
+        imageableId
+        */
+
+        if (!review) {
+            const err = new Error('The specified spot does not exist');
+            err.status = 404
+            res.json({
+                message: err.message,
+                code: err.status
+            });
+        } else if (spot.ownerId !== userId) {
+            const err = new Error('You are not authorized to add an image to this spot');
+            err.status = 403
+            res.json({
+                message: err.message,
+                code: err.status
+            })
+        } else {
+            const { url } = req.body;
+            const image = await Image.create({
+                url,
+                imageableType: 'Review',
+                imageableId: spotId
+            })
+            res.json(image)
+        }
+        //end of async
     }
 );
 
